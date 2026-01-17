aabe774f4290b4f4ef3cf0197b47896851112077
qcheng1-prog <qcheng1@charlotte.edu>
2026-01-17 11:40:49 -0500

    Comment out code retrieval from query parameters
    Comment out the retrieval of 'code' from query parameters.

--- auth/providers/google.py
+++ auth/providers/google.py
@@ -34,7 +34,7 @@ class GoogleProvider(OAuthProvider):
         #if "code" not in q or "state" not in q:
         #    return None
         q = st.query_params #st.experimental_get_query_params()
-        code = st.experimental_get_query_params().get("code", [None])[0]
+        #code = st.experimental_get_query_params().get("code", [None])[0]
         returned_state = q.get("state")
         code = q.get("code")
         
